0b55c733634eab33b24fd554a55fc3e6cc673afe
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-08-29 10:53:23 +0300

    first commit

---
+++ src/Page/Home/Home.tsx
@@ -0,0 +1,12 @@
+import stules from './Home.module.scss'
+
+
+const Home = () => {
+    return (
+        <div className={stules.container}>
+            dsad
+        </div>
+    )
+}
+
+export default Home
